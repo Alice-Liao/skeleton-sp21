d527014c9b3739877863733fa6f9e274f75dc81c
Alice-Liao <309853077@qq.com>
2022-11-21 10:43:27 -0800

    finished lab4

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -2,6 +2,7 @@ package flik;
 
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
+        /**
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
@@ -10,5 +11,21 @@ public class HorribleSteve {
             }
         }
         System.out.println("i is " + i);
+         **/
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < 500; i++) {
+            a = i;
+            b = i;
+            if (!Flik.isSameNumber(a, b)) {
+                throw new Exception(
+                        String.format("a:%d not same as b:%d ??", a, b));
+
+            }
+
+        }System.out.println("a is " + a );
+
+
     }
+
 }
---
+++ lab4/flik/flikTest.java
@@ -0,0 +1,29 @@
+package flik;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+//当i变成200的时候，程序就无法跑下去了。 是Flikfunction的问题？还是test写的有问题。 到128的时候有问题
+//The obvious conclusion: never rely on two references being identical, always compare them with .equals() method.
+public class flikTest {
+    @Test
+    public void testFlik(){
+        int a;
+        int b;
+        for (int i = 0; i <= 200; i++){
+            a = i;
+            b = i;
+            assertTrue(Flik.isSameNumber(a, b));
+        }
+    }
+    @Test
+    public void testFlik2(){
+        int a;
+        int b;
+        a = 128;
+        b = 128;
+            assertTrue(Flik.isSameNumber(a, b));
+        }
+    }
+
